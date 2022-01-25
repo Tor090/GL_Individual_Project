c4cb4d0b41395485467ef557ef1ba6245c9b4c3b
oleh.mysak <cufttt@gmail.com>
2022-01-25 12:35:20 +0200

    Front for home screen screen

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,61 +1,103 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:individual_project/home/widget/ganre_display/ganre_page.dart';
 import 'package:individual_project/home/widget/movie/movie_page.dart';
 import 'package:individual_project/search/view/search_page.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({Key? key}) : super(key: key);
 
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _search(context),
-      ),
-      body: _homebody(),
+
+      body: _homebody(context),
     );
   }
 
   Widget _search(BuildContext context){
-    String searchText = '';
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(left: 10),
+        height: 50,
+        width: MediaQuery.of(context).size.width/1.2,
+        //color: Colors.white,
+        child: Expanded(
+              child: TextField(
+                autofocus: false,
+                //cursorColor: Colors.white,
+                showCursor: false,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  suffixIcon: IconButton(
+                    onPressed: (){
+                      _controller.text.isNotEmpty ?
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SearchMoviePage(query: _controller.text),
+                        ),
+                      ) : {ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(content: Text('Query is empty')))};
+                    },
+                    icon: Icon(Icons.search),
+                    color: Colors.white,
+                  ),
+                ),
+                controller: _controller,
+              ),
             ),
-            onChanged: (text){searchText = text;
-            print(searchText);},
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(25),
+          ),
+          border: Border.all(
+            width: 1,
           ),
         ),
-        IconButton(onPressed: (){Navigator.push(
-            context,
-            MaterialPageRoute(
-            builder: (context) => SearchMoviePage(query: searchText),
-          ),
-        );
-        },
-            icon: Icon(Icons.search)),
-      ],
+      ),
     );
   }
 
-  Widget _homebody(){
+  Widget _homebody(BuildContext context){
     return SingleChildScrollView(
-      child: Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text('Now Playing', style: TextStyle(fontSize: 25),),
-                  MoviePage(selectedGanre: 0,query: 'now_playing',height: 2,width: 1.7,),
-                  Text('Popular', style: TextStyle(fontSize: 20),),
-                  MoviePage(selectedGanre: 0,query: 'popular',height: 3,width: 1.7,),
-                  GanrePage(height: 3,width: 1.7),
-                ],
-        ),
-      ),
+          child: Stack(
+            children: [
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 15.0,sigmaY: 15.0),
+                child: Image.asset(
+                  'assets/images/moviedb512.png',
+                ),
+              ),
+              SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _search(context),
+                    SizedBox(height: 50,),
+                    Text(
+                      'Now Playing',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+
+                      ),
+                    ),
+                    MoviePage(selectedGanre: 0,query: 'now_playing',height: 2,width: 1.7,),
+                    Text('Popular', style: TextStyle(fontSize: 20),),
+                    MoviePage(selectedGanre: 0,query: 'popular',height: 3,width: 1.7,),
+                    GanrePage(height: 3,width: 1.7),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
     );
   }
 
